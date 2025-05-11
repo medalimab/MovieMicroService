6eec9ee6c2a7e36f72ef3a943f73e2f952568c4a
dali <mabroukdali1925@gmail.com>
2025-05-12 00:28:32 +0200

    feat(graphql): add resolvers for Movie and TVShow gRPC services

---
+++ resolvers.js
@@ -0,0 +1,110 @@
+const grpc = require('@grpc/grpc-js');
+const protoLoader = require('@grpc/proto-loader');
+
+const movieProtoPath = 'movie.proto';
+const tvShowProtoPath = 'tvShow.proto';
+
+const movieProtoDefinition = protoLoader.loadSync(movieProtoPath, {
+  keepCase: true,
+  longs: String,
+  enums: String,
+  defaults: true,
+  oneofs: true,
+});
+const tvShowProtoDefinition = protoLoader.loadSync(tvShowProtoPath, {
+  keepCase: true,
+  longs: String,
+  enums: String,
+  defaults: true,
+  oneofs: true,
+});
+
+const movieProto = grpc.loadPackageDefinition(movieProtoDefinition).movie;
+const tvShowProto = grpc.loadPackageDefinition(tvShowProtoDefinition).tvShow;
+
+const resolvers = {
+  Query: {
+    movie: (_, { id }) => {
+      const client = new movieProto.MovieService(
+        'localhost:50051',
+        grpc.credentials.createInsecure()
+      );
+      return new Promise((resolve, reject) => {
+        client.getMovie({ movie_id: id }, (err, response) => {
+          if (err) reject(err);
+          else resolve(response.movie);
+        });
+      });
+    },
+    movies: (_, { query = '' }) => {
+      const client = new movieProto.MovieService(
+        'localhost:50051',
+        grpc.credentials.createInsecure()
+      );
+      return new Promise((resolve, reject) => {
+        client.searchMovies({ query }, (err, response) => {
+          if (err) reject(err);
+          else resolve(response.movies);
+        });
+      });
+    },
+    tvShow: (_, { id }) => {
+      const client = new tvShowProto.TVShowService(
+        'localhost:50052',
+        grpc.credentials.createInsecure()
+      );
+      return new Promise((resolve, reject) => {
+        client.getTvshow({ tv_show_id: id }, (err, response) => {
+          if (err) reject(err);
+          else resolve(response.tv_show);
+        });
+      });
+    },
+    tvShows: (_, { query = '' }) => {
+      const client = new tvShowProto.TVShowService(
+        'localhost:50052',
+        grpc.credentials.createInsecure()
+      );
+      return new Promise((resolve, reject) => {
+        client.searchTvshows({ query }, (err, response) => {
+          if (err) reject(err);
+          else resolve(response.tv_shows);
+        });
+      });
+    },
+  },
+  Mutation: {
+    createMovie: (_, { id, title, description }) => {
+      const client = new movieProto.MovieService(
+        'localhost:50051',
+        grpc.credentials.createInsecure()
+      );
+      return new Promise((resolve, reject) => {
+        client.createMovie(
+          { movie: { id, title, description } },
+          (err, response) => {
+            if (err) reject(err);
+            else resolve(response.movie);
+          }
+        );
+      });
+    },
+    createTVShow: (_, { id, title, description }) => {
+      const client = new tvShowProto.TVShowService(
+        'localhost:50052',
+        grpc.credentials.createInsecure()
+      );
+      return new Promise((resolve, reject) => {
+        client.createTvshow(
+          { tv_show: { id, title, description } },
+          (err, response) => {
+            if (err) reject(err);
+            else resolve(response.tv_show);
+          }
+        );
+      });
+    },
+  },
+};
+
+module.exports = resolvers;
